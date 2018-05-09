e2e2d2c94fa835f1703f7f75f6bdf7cbaaba9a38
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-09 19:23:17 +0200

    modify after codereview

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,14 +6,12 @@ value = (a * a) - (2 * a * b) + (b * b);
 
 console.log(value);
 
-if (value >0) {
+if (value > 0) {
 	console.log('wynik dodatni');
-} else if (value <0) {
+} else if (value < 0) {
 	console.log('wynik ujemny');
-}
-else if (value ==NaN) {
+} else if (isNaN(value)) {
 	console.log('isNaN');
-}
-else {
+} else {
 	console.log('zero');
 }
